1105926d4a9f491753554abe135df91897e2e724
Atique Ahmad <65412984+Atique22@users.noreply.github.com>
2022-03-08 12:45:04 +0500

    contact.php

--- contact.php
+++ contact.php
@@ -1,7 +1,4 @@
 
-
-
-
 <?php
 
         if(isset($_POST['submit'])){
@@ -10,14 +7,14 @@
             $NAME = $_POST['Name'];
             $PROJECT = $_POST['Project'];
             $subject = "Form submission";
-            $subject2 = "Copy of your form submission";
+            //$subject2 = "Copy of your form submission";
             $message = $NAME . " " . " wrote the following:" . "\n\n"."Project Name".$PROJECT."\n\n"."Message:" .$_POST['Message'];
-            $message2 = "Here is a copy of your message " . $NAME . "\n\n" ."Project Name".$PROJECT."\n\n"."Message:". $_POST['Message'];
+            //$message2 = "Here is a copy of your message " . $NAME . "\n\n" ."Project Name".$PROJECT."\n\n"."Message:". $_POST['Message'];
 
             $headers = "From:" .$from;
-            $headers2 = "From:" .$to;
+            //$headers2 = "From:" .$to;
             mail($to,$subject,$message,$headers);
-            mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+            //mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
             echo "Mail Sent. Thank you " . $NAME . ", we will contact you shortly.";
             // You can also use header('Location: thank_you.php'); to redirect to another page.
         }
@@ -25,44 +22,3 @@
         echo "<br> Plz fill out complete form! <br>";
       }
 ?>
-
-<!-- 
-//error_reporting(0);
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "atique_portfolio";
-
-// Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-
-
-  // collect value of input field
-            $NAME = $_POST['Name'];
-            $EMAIL = $_POST['Email'];
-            $PROJECT = $_POST['Project'];
-            $MESSAGE = $_POST['Message'];
-
-     if (!empty($NAME) || !empty($MESSAGE) ||  !empty($EMAIL) || !empty($PROJECT) ) {
-
-///inserted data
-         $sql = "INSERT INTO contact VALUES ('','$NAME', '$EMAIL', '$PROJECT','$MESSAGE')";
-
-// sql data
-            if ($conn->query($sql) === TRUE) {
-                echo "<br> successfully! <br>";
-             }
-            else {
-                    echo "Error: " . $sql . "<br>" . $conn->error;
-                }
-
-      }
-      else {
-        echo "<br> Plz fill out complete form! <br>";
-      }
-
-$conn->close(); -->
